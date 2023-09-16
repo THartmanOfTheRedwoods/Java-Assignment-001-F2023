b7730e19043c67fb69e0b122d3d6793b08c3344c
MJFracess <mjfracess@gmail.com>
2023-09-15 19:37:42 -0700

    MJ Fracess CIS 12 Assignment 001

--- src/AlanTuring.java
+++ src/AlanTuring.java
@@ -1,9 +1,15 @@
 public class AlanTuring {
     /**
+     * @author Trevor Hartman
+     * @author MJ Fracess
+     *
+     * @since Version 1.0
+     *
      * Prints Alan Turing to the console
      * @param args Command line arguments [The source file path, The target file path, ...]
      **/
     public static void main(String[] args) {
-        // Write your program here
+    //Here goes nothing
+        System.out.println("Alan Turing");
     }
 }
