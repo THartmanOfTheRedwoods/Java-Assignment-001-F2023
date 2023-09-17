15b4b45b94751e5fd485d8670c9829d30f373b4d
fires <mlarose859@mycr.redwoods.edu>
2023-09-16 17:53:53 -0700

    Completed Assignment 001

--- src/JurassicPark.java
+++ src/JurassicPark.java
@@ -1,9 +1,21 @@
+/**
+ *
+ * @author Trevor Hartman
+ * @author Miaka LaRose
+ *
+ * @since Version 1.0
+ *
+ */
 public class JurassicPark {
     /**
      * Prints famous Jurasic Park quote to console with only 1 sout statement.
      * @param args Command line arguments [The source file path, The target file path, ...]
      **/
     public static void main(String[] args) {
+        System.out.println("""
+                "Life, uh,
+                 finds a way."
+                —Ian Malcolm""");
         // Write your program here
     }
 }
